18616dbd39a9a843454c93b8e794b37ea8ccbf3d
Conrad Etherington <conrad.etherington@gmail.com>
2023-11-22 23:36:09 -0700

    all tests passing

--- lib/list_crud.py
+++ lib/list_crud.py
@@ -1,26 +1,44 @@
 def create_an_empty_list():
-    return None
+    return list()
 
 def create_a_list():
-    return None
+    return list([1,2,3,4])
 
 def add_element_to_end_of_list(l, element):
-    return None
+    out = list(l)
+    out.append(element)
+    return out
 
 def add_element_to_start_of_list(l, element):
-    return None
+    out = list(l)
+    out.insert(0,element)
+    return out
 
 def remove_element_from_end_of_list(l):
-    return None
+    # listo = list(l)
+    # listo.pop()
+    list_length = len(l)
+    return [li for idx, li in enumerate(l) if idx < list_length -1]
+    # return listo
+    #alternate solution
 
 def remove_element_from_start_of_list(l):
-    return None
-
+    # return [li for idx, li in enumerate(l) if idx > 0]
+    #alternate solution
+    listo = list(l)
+    listo.pop(0)
+    return listo
 def retrieve_first_element_from_list(l):
-    return None
+    # return [li for idx, li in enumerate(l) if idx == 0][0]
+    #alternate solution
+    listo = list(l)
+    return listo[0]
 
 def retrieve_element_from_index(l, index):
-    return None
+    listo = list(l)
+    return listo[index]
+    
 
 def retrieve_last_element_from_list(l):
-    return None
+    listo = list(l)
+    return listo[-1]
